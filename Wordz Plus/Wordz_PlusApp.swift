3d856d22e7f5fef20231e90b5084bb7167853486
Teo <teo.miscia@gmail.com>
2025-06-22 23:41:04 -0700

    Initial Commit

---
+++ Wordz Plus/Wordz_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordz_PlusApp.swift
+//  Wordz Plus
+//
+//  Created by Teo Miscia on 22/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Wordz_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
